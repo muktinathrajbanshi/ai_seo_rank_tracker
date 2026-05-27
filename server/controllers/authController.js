c6c01a9acffa7ffa487c1311938449044019be73
unknown <muktinathrajbanshi786@gmail.com>
2026-05-27 20:04:46 +0545

    la khai ta

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,5 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
 
 // Register user
 export const register = async (req, res) => {
@@ -6,6 +8,17 @@ export const register = async (req, res) => {
 
         if (!name, !email, !password) return res.status(400).json({
             success: false, message: "All fields are required"});
+
+    // Check if user exists
+    const existingUser = await User.findOne({email})    
+    if(existingUser) return res.status(400).json({
+        success: false, message: "User already exists"});
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, await bcrypt.genSalt(10))
+
+    
+
     } catch (error) {
         
     }
