a64df45cd1fac42ad7467ccd4d1b9ca6c588a2e3
unknown <muktinathrajbanshi786@gmail.com>
2026-05-27 21:48:21 +0545

    aja lai yati yar nidra lago

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -12,7 +12,7 @@ export const register = async (req, res) => {
     try {
         const {name, email, password} = req.body;
 
-        if (!name, !email, !password) return res.status(400).json({
+        if (!name || !email || !password) return res.status(400).json({
             success: false, message: "All fields are required"});
 
     // Check if user exists
@@ -32,6 +32,44 @@ export const register = async (req, res) => {
 
 
     } catch (error) {
+        console.error("Register error:", error.message);
+        res.status(500).json({success: false, message: "Server error"})
+    }
+}
+
+
+//Login user
+export const register = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        if (!email || !password) return res.status(400).json({
+            success: false, message: "All fields are required"});
+
+    // Find user
+    const user = await User.findOne({email})    
+    if(!user) return res.status(400).json({
+        success: false, message: "Invalid credentials"});
+
+    // Check password 
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch) {
         
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, await bcrypt.genSalt(10))
+
+    // Create user
+    const user = await User.create({name, email, password: hashedPassword})
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({success: true, token, user})
+
+
+    } catch (error) {
+        console.error("Register error:", error.message);
+        res.status(500).json({success: false, message: "Server error"})
     }
 }
