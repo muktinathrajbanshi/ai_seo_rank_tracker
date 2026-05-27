1b6b30d8cbb0dcd95173e41f8d6063e9455964b2
unknown <muktinathrajbanshi786@gmail.com>
2026-05-27 19:33:20 +0545

    wah vidu kiya baat hain

---
+++ server/controllers/authController.js
@@ -0,0 +1,12 @@
+
+// Register user
+export const register = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        if (!name, !email, !password) return res.status(400).json({
+            success: false, message: "All fields are required"});
+    } catch (error) {
+        
+    }
+}
