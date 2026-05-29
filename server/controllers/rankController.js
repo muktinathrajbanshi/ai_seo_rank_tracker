0b0cf20b1db8569ca952354ec369061b208f2eb5
unknown <muktinathrajbanshi786@gmail.com>
2026-05-29 13:04:13 +0545

    hait yar k vanum

--- server/controllers/rankController.js
+++ server/controllers/rankController.js
@@ -5,6 +5,26 @@ export const addKeyword = async (req, res) => {
 }
 
 // Add a keyword to track
-export const addKeyword = async (req, res) => {
+export const getKeywords = async (req, res) => {
+    
+}
+
+// Get single keyword with full history
+export const getKeyword = async (req, res) => {
+    
+}
+
+// Manually refresh a keyword ranking
+export const refreshKeyword = async (req, res) => {
+    
+}
+
+// Delete keyword tracking
+export const deleteKeyword = async (req, res) => {
+    
+}
+
+// Toggle tracking active/inactive
+export const toggleTracking = async (req, res) => {
     
 }
