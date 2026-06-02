c469c2a7be57a79c7c7dfbf5019fa326fc6405c0
unknown <muktinathrajbanshi786@gmail.com>
2026-06-02 11:39:56 +0545

    hait mama garmi ho raichha

---
+++ server/controllers/analysisController.js
@@ -0,0 +1,20 @@
+
+// Analyze a URL 
+export const analyzeUrl = async (req, res) => {
+    
+}
+
+// Get analysis by ID
+export const getAnalysis = async (req, res) => {
+    
+}
+
+// Get all analyses for 
+export const getAnalyses = async (req, res) => {
+    
+}
+
+// Delete analysis
+export const deleteAnalysis = async (req, res) => {
+    
+}
