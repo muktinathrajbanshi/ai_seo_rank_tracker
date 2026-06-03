8324b0f877a6497fc22655ea3cdc6e4e898cf8f9
unknown <muktinathrajbanshi786@gmail.com>
2026-06-03 12:08:12 +0545

    hait k saro garmi yar

--- server/controllers/analysisController.js
+++ server/controllers/analysisController.js
@@ -57,6 +57,9 @@ export const analyzeUrl = async (req, res) => {
             analysis.loadTime = scrapeResult.data.loadTime || 0;
             analysis.pageSize = scrapeResult.data.pageSize || 0;
             analysis.wordCount = scrapeResult.data.wordCount || 0;
+            analysis.status = "completed";
+
+            await analysis.save();
 
 
         } catch (bgeError) {
@@ -80,12 +83,45 @@ export const analyzeUrl = async (req, res) => {
 
 // Get analysis by ID
 export const getAnalysis = async (req, res) => {
-    
+    try {
+        const analysis = await Analysis.findOne({_id: req.params.id, userId: req.userId})
+
+        if(!analysis) return res.status(404).json({ success: false, message: "Analysis not found" });
+
+        res.json({ success: true, analysis });
+
+    } catch (error) {
+        console.error("Get analysis error:", error.message);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
 }
 
-// Get all analyses for 
+// Get all analyses for user
 export const getAnalyses = async (req, res) => {
-    
+    try {
+
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+
+        const analyses = await (await Analysis
+            .find({userId: req.userId}))
+            .toSorted({createdAt: -1})
+            .skip(skip)
+            .limit(limit)
+            .select("-issues -keywords");
+
+        const total = await Analysis.countDocuments({userId: req.userId})
+
+        res.json({ 
+            success: true, analyses, 
+            pagination: {page, limit, total, 
+            pages: Math.ceil(total / limit)} });
+
+    } catch (error) {
+        console.error("Get analysis error:", error.message);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
 }
 
 // Delete analysis
