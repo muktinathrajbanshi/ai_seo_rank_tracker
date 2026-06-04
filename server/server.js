c66280f03b8599519122d5fe11a2466c8728b756
unknown <muktinathrajbanshi786@gmail.com>
2026-06-04 09:21:27 +0545

    so let's get started muktinath

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
 import rankRouter from "./routes/rankRoutes.js";
+import analysisRouter from "./routes/analysisRoutes.js";
 
 connectDB()
 
@@ -16,6 +17,7 @@ app.use(express.json())
 app.get("/", (req, res) => res.send("Server is running"))
 app.use("/api/auth", authRouter)
 app.use("/api/rank", rankRouter)
+app.use("/api/analysis", analysisRouter)
 
 const PORT = process.env.PORT || 5000;
 
